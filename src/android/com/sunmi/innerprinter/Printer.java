c2f6d931baf05122067f4c5e035df236eb615db3
Stefan Pilz <stefan.pilz@apro.at>
2023-10-06 08:04:14 +0200

    readd inner result callback for qr code print

--- src/android/com/sunmi/innerprinter/Printer.java
+++ src/android/com/sunmi/innerprinter/Printer.java
@@ -739,8 +739,31 @@ public class Printer extends CordovaPlugin {
         @Override
         public void run() {
           try {
-            printerService.printQRCode(d, size, level, null);
-            callbackContext.success("");
+            printerService.printQRCode(d, size, level, new InnerResultCallback() {
+              @Override
+              public void onRunResult(boolean isSuccess) {
+                if (isSuccess) {
+                  callbackContext.success("");
+                } else {
+                  callbackContext.error(isSuccess + "");
+                }
+              }
+
+              @Override
+              public void onReturnString(String result) {
+                callbackContext.success(result);
+              }
+
+              @Override
+              public void onRaiseException(int code, String msg) {
+                callbackContext.error(msg);
+              }
+
+              @Override
+              public void onPrintResult(int code, String msg) {
+                  callbackContext.success(msg);
+              }
+            });
           } catch (Exception e) {
             e.printStackTrace();
             Log.i(TAG, "ERROR: " + e.getMessage());
